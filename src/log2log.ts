e029fc526b6fc727d2bdb5f0925307775e41093d
Matthew Weidner <17693586+mweidner037@users.noreply.github.com>
2026-06-16 20:52:23 -0500

    TypeToModel ideas

---
+++ src/log2log.ts
@@ -0,0 +1,29 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+import { BaseValue, DefModel, MutableModel } from "./model";
+
+export type BaseTypeToModel = {
+  [K in string]: DefModel<
+    BaseValue<K>,
+    object,
+    MutableModel<BaseValue<K>, object>
+  >;
+};
+
+export type ValueType<
+  TypeToModel extends BaseTypeToModel,
+  K extends keyof TypeToModel,
+> = TypeToModel[K] extends DefModel<infer V, any, any> ? V : never;
+
+export interface ValueStore<TypeToModel extends BaseTypeToModel> {
+  get<K extends keyof TypeToModel>(
+    type: K,
+    id: string,
+  ): ValueType<TypeToModel, K> | null;
+
+  set<K extends keyof TypeToModel>(value: ValueType<TypeToModel, K>): void;
+}
+
+export class Log2Log<TypeToModel extends BaseTypeToModel> {
+  constructor(readonly store: ValueStore<TypeToModel>) {}
+}
---
+++ src/model.ts
@@ -0,0 +1,21 @@
+export interface BaseValue<K = string> {
+  readonly type: K;
+  readonly id: string;
+}
+
+export interface MutableModel<V, U> {
+  beginTransaction(): void;
+  rollback(): void;
+  commit(): U[];
+  toImmutable(): V;
+}
+
+export interface DefModel<
+  V extends BaseValue,
+  U extends object,
+  M extends MutableModel<V, U>,
+> {
+  toMutable(value: V): M;
+  applyUpdates(value: V, updates: U[]): V;
+  compactUpdates(updates: U[]): U[];
+}
