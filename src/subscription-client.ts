0473489421a3a9891c5e90d06400859a17b26c37
Matthew Weidner <17693586+mweidner037@users.noreply.github.com>
2026-06-23 21:59:55 -0500

    working on deltas

--- src/subscription-client.ts
+++ src/subscription-client.ts
@@ -1,15 +1,17 @@
 import { BaseTypeToModel } from "./model";
 import { ReconciliationReplica } from "./reconciliation-replica";
 import { BiMap } from "./util/bi-map";
+import { SubscriptionDelta } from "./util/subscription-delta";
 
 // TODO: Reactivity for isKnown changes from either cause
-// TODO: Rerun local mutations when we receive subscription updates?
-// TODO: Does unsubscribing immediately drop a value, or do we wait for the
-// server's response? If the former, do we rerun local mutations?
+// TODO: Rerun local mutations when we receive subscription updates.
+// But on the server, wait to send sub deletes until you have a real change.
 
 export class SubscriptionClient<TTM extends BaseTypeToModel> {
   private readonly activeSubscriptions = new BiMap<TTM, true>();
   private readonly requestedSubscriptions = new BiMap<TTM, true>();
+
+  private pendingDelta = new SubscriptionDelta<TTM>();
 
   constructor(
     readonly typeToModel: TTM,
@@ -22,5 +24,28 @@ export class SubscriptionClient<TTM extends BaseTypeToModel> {
     if (this.replica.optimisticDiff.sets.has(type, id)) return true;
     if (this.replica.optimisticDiff.deletes.has(type, id)) return true;
     return false;
+  }
+
+  subscribe<K extends keyof TTM>(type: K, id: string): void {
+    if (!this.requestedSubscriptions.has(type, id)) {
+      this.pendingDelta.add(type, id);
+      this.requestedSubscriptions.set(type, id, true);
+    }
+  }
+
+  unsubscribe<K extends keyof TTM>(type: K, id: string): void {
+    if (this.requestedSubscriptions.has(type, id)) {
+      this.pendingDelta.delete(type, id);
+      this.requestedSubscriptions.delete(type, id);
+    }
+  }
+
+  /**
+   * Returns the current built-up SubscriptionDelta and resets it.
+   */
+  getAndResetDelta(): SubscriptionDelta<TTM> {
+    const ans = this.pendingDelta;
+    this.pendingDelta = new SubscriptionDelta();
+    return ans;
   }
 }
--- src/util/subscription-delta.ts
+++ src/util/subscription-delta.ts
@@ -1,10 +1,26 @@
+import * as z from "zod";
 import { BaseTypeToModel } from "../model";
 import { BiMap } from "./bi-map";
 
+export type SavedSubscriptionDelta = {
+  /** Adds, mapping type to an array of ids. */
+  adds: Record<string, string[]>;
+  /** Deletes, mapping type to an array of ids. */
+  deletes: Record<string, string[]>;
+};
+
+/**
+ * Zod schema for SavedSubscriptionDelta.
+ */
+export const zSubscriptionDelta: z.ZodType<SavedSubscriptionDelta> = z.object({
+  adds: z.record(z.string(), z.array(z.string())),
+  deletes: z.record(z.string(), z.array(z.string())),
+});
+
 export class SubscriptionDelta<TTM extends BaseTypeToModel> {
   constructor(
-    readonly adds: BiMap<TTM, true>,
-    readonly deletes: BiMap<TTM, true>
+    readonly adds = new BiMap<TTM, true>(),
+    readonly deletes = new BiMap<TTM, true>()
   ) {}
 
   /**
@@ -12,12 +28,56 @@ export class SubscriptionDelta<TTM extends BaseTypeToModel> {
    */
   apply(delta: SubscriptionDelta<TTM>): void {
     for (const [type, id] of delta.adds.entries()) {
-      this.adds.set(type, id, true);
-      this.deletes.delete(type, id);
+      this.add(type, id);
     }
     for (const [type, id] of delta.deletes.entries()) {
-      this.deletes.set(type, id, true);
-      this.adds.delete(type, id);
+      this.add(type, id);
     }
+  }
+
+  add<K extends keyof TTM>(type: K, id: string): void {
+    this.adds.set(type, id, true);
+    this.deletes.delete(type, id);
+  }
+
+  delete<K extends keyof TTM>(type: K, id: string): void {
+    this.deletes.set(type, id, true);
+    this.adds.delete(type, id);
+  }
+
+  save(): SavedSubscriptionDelta {
+    const adds: Record<string, string[]> = {};
+    for (const [type, id] of this.adds.entries()) {
+      const ids = adds[type];
+      if (ids === undefined) adds[type] = [id];
+      else ids.push(id);
+    }
+
+    const deletes: Record<string, string[]> = {};
+    for (const [type, id] of this.deletes.entries()) {
+      const ids = deletes[type];
+      if (ids === undefined) deletes[type] = [id];
+      else ids.push(id);
+    }
+
+    return { adds, deletes };
+  }
+
+  static load<TTM extends BaseTypeToModel>(
+    json: object
+  ): SubscriptionDelta<TTM> {
+    const saved = json as SavedSubscriptionDelta;
+
+    const adds = new BiMap<TTM, true>();
+    for (const type of Object.keys(saved.adds)) {
+      for (const id of saved.adds[type]) adds.set(type, id, true);
+    }
+
+    const deletes = new BiMap<TTM, true>();
+    for (const type of Object.keys(saved.deletes)) {
+      for (const id of saved.deletes[type]) deletes.set(type, id, true);
+    }
+
+    return new SubscriptionDelta(adds, deletes);
   }
 }
